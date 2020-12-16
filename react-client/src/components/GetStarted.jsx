1a4f945ee1339708b615d5953c01234879aa81c0
Lucy Armstrong <lucyarmstrong45@gmail.com>
2020-12-16 11:35:35 -0600

    homepage built except pictures

---
+++ react-client/src/components/GetStarted.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const GetStarted = ()=> {
+  return (
+    <Button variant="dark">Get Started</Button>
+  );
+};
+
+export default GetStarted;
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -4,6 +4,7 @@ import QueryForm from './components/QueryForm.jsx';
 import RecipeList from './components/RecipeList.jsx';
 import sampleData from './sampledata.jsx';
 import { Container, Row, Col } from 'react-bootstrap';
+import GetStarted from './components/GetStarted.jsx';
 
 
 class App extends React.Component {
@@ -16,15 +17,23 @@ class App extends React.Component {
 
   render () {
     return (
-      <Container>
-        <Row>
-          <Col>
-            <div>
-              <h1>What to Bring</h1>
-            </div>
-          </Col>
+      <>
+        <Row style={{justifyContent: 'center', fontFamily: 'candara', fontSize: '30px' }}>
+          ? What to Bring ?
         </Row>
-      </Container>
+        <Row style={{backgroundColor: '#000000'}}>
+            hi
+        </Row>
+        <Row style={{justifyContent: 'center', fontFamily: 'cambira', fontSize: '60px'}}>
+            Easy Recipes in Seconds!
+        </Row>
+        <Row style={{justifyContent: 'center', fontFamily: 'cambira', fontSize: '20px'}}>
+            Cooking for Friends should be fun and stress-free!
+        </Row>
+        <Row style= {{justifyContent: 'center'}}>
+          <GetStarted/>
+        </Row>
+      </>
     );
   }
 }
